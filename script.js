ada4ae7cc1100937f2c450f7c62177910c81deae
willy <willygodwin47@gmail.com>
2020-12-10 18:09:18 +0800

    Updated input text box

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
+//Global Variables
+let currentHour = moment().format('H');
+
 // TODO1: Fetch the time and date for heading from moment.js
 //Fetch the currentDay element
 currentDay = $("#currentDay");
 
-day = moment().format("dddd") // => ('Monday' , 'Tuesday' ----)
-date = moment().format("Do")
-month = moment().format("MMMM") // => ('January','February.....)
-year = moment().format("YYYY") // => ('2012','2013' ...)  
+day = moment().format("dddd")     // => ('Monday' , 'Tuesday' ----)
+date = moment().format("Do")      // => ('1st' , '2nd' ----)
+month = moment().format("MMMM")   // => ('January','February.....)
+year = moment().format("YYYY")    // => ('2012','2013' ...)  
 
 currentDay.text(day + " " + date + " " + month + " " + year)
 
 
 // TODO2: Build Array to store the data of the planner length 9
 // Fetch the stored plans from localStorage
-let plansStored = JSON.parse(localStorage.getItem("plansStored"));
-let plansArray = [];
+let todoStored = JSON.parse(localStorage.getItem("todoStored"));
 
-if (plansStored !== null) {
-    plansArray = storedPlans;
+
+if (todoStored !== null) {
+    plansArray = todoStored;
   } else {
-    // this should only occur on first time the app is loaded in the browser
-    // Array to store daily plans 
+    // initialise array to store daily plans only requried on first ever creation
     plansArray = []; 
     plansArray[0] = "Good Morning";
   }
@@ -34,7 +36,7 @@ function populatePlanner(){
 
         let hour = i + 9;
 
-        //Build row componetns
+        //Build row elements
         let rowDiv = $("<div>"); 
         rowDiv.addClass("row");
         rowDiv.addClass("plannerRow");
@@ -67,16 +69,16 @@ function populatePlanner(){
         // insert into col inset into timebox
         rowDiv.append(timeDivCol);
         timeDivCol.append(timeBoxSpn);
-        // STOP building Time box portion of row
+        
 
-        // START building input portion of row
+        
         // build row components
         let dailyPlanInput = $("<input>");
 
         dailyPlanInput.attr("hour-index", i);
         dailyPlanInput.attr("id","input-" + i);
         dailyPlanInput.attr("type","text");
-        dailyPlanInput.attr("class","dailyPlan");
+        dailyPlanInput.attr("class","inputBox");
 
         // access index from data array for hour 
         dailyPlanInput.val( plansArray[i] );
@@ -96,26 +98,53 @@ function populatePlanner(){
         let saveBtn = $("<button>");
         saveBtn.attr("id","saveid-" + i);
         saveBtn.attr("save-id", i);
-        saveBtn.attr("class","far fa-save saveIcon");
+        saveBtn.attr("class","far fa-save saveIcon saveBtn");
         
         // add col width and row component to row
         rowDiv.append(saveDivCol);
         saveDivCol.append(saveBtn);
-        // STOP building save portion of row
-
-        // set row color based on time
-        // updateRowColor(rowDiv, hour);
+        
 
         plansContainer.append(rowDiv);
 
+        updateRowColor(rowDiv, hour)
+
     }
 }
-// TODO4: Function that creates 9 inputs associated with the time slots
-// TODO5: Save button that stores the input to the local storage
-// TODO6: Change the row colour based on the time 
+
+// TODO4: Change the row colour based on the time 
 function updateRowColor(row, time){
   
+  if(time < currentHour){
+    row.css("background-color","lightgrey")
+  }
+  else if(time > currentHour)
+  {
+    row.css("background-color","lightgreen")
+  }
+  else {
+    row.css("background-color","red")
+  }
 }
+
+// TODO5: Save button that stores the input to the local storage
+$(document).on("click", "button", function(event){
+  event.preventDefault();  
+ 
+  let index = $(this).attr("save-id");
+  console.log(index);
+  let inputID = "#input-" + index;
+
+  console.log(inputID);
+
+  plansArray[index] = $(inputID).val();
+
+  console.log(plansArray)
+
+  localStorage.setItem("todoStored", JSON.stringify(plansArray));
+  });  
+
+
 
 populatePlanner();
 
